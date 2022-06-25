eaa598a45c8c3237b7355df8984d0227e0f94dc9
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2022-06-25 10:49:58 -0500

    kk

--- checkin/checkin/settings.py
+++ checkin/checkin/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
